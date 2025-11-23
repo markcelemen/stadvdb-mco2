ed1df0f73d1db0660990cec96dd4d7695c79091e
IceKane <bkgonzales157@gmail.com>
2025-11-23 12:53:19 +0800

    feat: added sql scripts for database schemas

---
+++ database/schema_olap.sql
@@ -0,0 +1,56 @@
+CREATE TABLE DimBuyer(
+    buyer_id int,
+    user_name varchar(100),
+    PRIMARY KEY (buyer_id)
+);
+
+CREATE TABLE DimSeller(
+    seller_id int,
+    user_name varchar(100),
+    PRIMARY KEY (seller_id)
+);
+
+CREATE TABLE DimProduct(
+    product_id int,
+    product_name varchar(100),
+    category varchar(100),
+    original_price decimal(8,2),
+    PRIMARY KEY (product_id)
+);
+
+CREATE TABLE DimTime(
+    time_id int,
+    t_hour int, --Range [0,23]
+    t_day int,
+    t_month int,
+    t_year int,
+    PRIMARY KEY (time_id)
+);
+
+CREATE TABLE DimFlashSale(
+    flash_sale_id int,
+    title varchar(100),
+    start_time int,
+    end_time int,
+    PRIMARY KEY (flash_sale_id)
+    FOREIGN KEY (start_time) REFERENCES DimTime(time_id),
+    FOREIGN KEY (end_time) REFERENCES DimTime(time_id)
+);
+
+CREATE TABLE FactOrders(
+    order_id int,
+    product_id int,
+    time_id int,
+    buyer_id int,
+    seller_id int,
+    flash_sale_id int,
+    quantity_sold int,
+    price_per_item decimal(8,2),
+    total_sale decimal(11,2),
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (product_id) REFERENCES DimProduct(product_id),
+    FOREIGN KEY (time_id) REFERENCES DimTime(time_id),
+    FOREIGN KEY (buyer_id) REFERENCES DimBuyer(buyer_id),
+    FOREIGN KEY (seller_id) REFERENCES DimSeller(seller_id),
+    FOREIGN KEY (flash_sale_id) REFERENCES DimFlashSale(flash_sale_id)
+);
---
+++ database/schema_oltp.sql
@@ -0,0 +1,58 @@
+CREATE TABLE Users(
+    user_id int,
+    user_name varchar(100),
+    email varchar(100),
+    user_pw varchar(100), --password
+    user_role ENUM('SELLER', 'BUYER'),
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE FlashSales(
+    flash_sale_id int,
+    start_time datetime,
+    end_time datetime,
+    PRIMARY KEY (flash_sale_id)
+);
+
+CREATE TABLE Products(
+    product_id int,
+    seller_id int,
+    product_name varchar(100),
+    category varchar(100),
+    product_desc varchar(256), --currently not in schema diagram
+    price decimal(8,2),
+    original_price decimal(8,2),
+    discount_rate decimal(4,2), --percentage
+    quantity_stock int,
+    flash_sale_id int,
+    PRIMARY KEY (product_id),
+    FOREIGN KEY (flash_sale_id) REFERENCES FlashSales(flash_sale_id)
+);
+
+CREATE TABLE Orders(
+    order_id int,
+    buyer_id int,
+    created_at datetime,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (buyer_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE OrderItems(
+    order_items_id int,
+    order_id int,
+    product_id int,
+    quantity_sold int,
+    PRIMARY KEY (order_items_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+CREATE TABLE CartItems(
+    cart_item_id int,
+    user_id int,
+    product_id int,
+    quantity_added int,
+    PRIMARY KEY (cart_item_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
