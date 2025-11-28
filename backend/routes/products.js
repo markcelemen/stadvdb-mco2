04d09b5eaf6e8ed363eaa07bc493af252121cdde
Kyle Gan <kyle_elijah_gan@dlsu.edu.ph>
2025-11-28 17:01:18 +0800

    fixed product.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db/pool');
+const { pool } = require('../db/pool');
 
 // Get all products (from database)
 router.get('/', async (req, res) => {
